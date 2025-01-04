213481f3d0bacdf850c916814445de2399cd19d8
Max Kyrychenko <iam.maxkyrychenko@gmail.com>
2025-01-04 15:58:58 +0200

    first commit

---
+++ src/test/java/com/example/mypianolist/MyPianoListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mypianolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyPianoListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
