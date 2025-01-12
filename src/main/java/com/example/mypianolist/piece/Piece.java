c2016e25161c25d40fb5c7dacc5d4fd515e6b2e8
Max Kyrychenko <iam.maxkyrychenko@gmail.com>
2025-01-12 09:18:52 +0200

    extend Piece entity
    - make fields non-nullable
    - initialize lists
    - add badic getters

--- src/main/java/com/example/mypianolist/piece/Piece.java
+++ src/main/java/com/example/mypianolist/piece/Piece.java
@@ -1,6 +1,7 @@
 package com.example.mypianolist.piece;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
@@ -9,6 +10,7 @@ import com.example.mypianolist.user.UserFavouritePiece;
 import com.example.mypianolist.user.UserPiece;
 
 import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -23,9 +25,16 @@ public class Piece {
 	@GeneratedValue(strategy = GenerationType.UUID)
 	private UUID id;
 
+	@Column(nullable = false)
 	private String title;
+
+	@Column(nullable = false)
 	private String description;
+
+	@Column(nullable = false)
 	private String image;
+
+	@Column(nullable = false)
 	private LocalDate composedAt;
 
 	@ManyToOne
@@ -37,8 +46,38 @@ public class Piece {
 	private PieceGenre genre;
 
 	@OneToMany(mappedBy = "piece", cascade = CascadeType.ALL, orphanRemoval = true)
-	private List<UserPiece> userPieces;
+	private List<UserPiece> userPieces = new ArrayList<>();
 
 	@OneToMany(mappedBy = "piece", cascade = CascadeType.ALL, orphanRemoval = true)
-	private List<UserFavouritePiece> favouritePieces;
+	private List<UserFavouritePiece> favouritePieces = new ArrayList<>();
+
+	public Piece() {
+	}
+
+	public Piece(String title, String description, String image, LocalDate composedAt) {
+		this.title = title;
+		this.description = description;
+		this.image = image;
+		this.composedAt = composedAt;
+	}
+
+	public UUID getId() {
+		return this.id;
+	}
+
+	public String getTitle() {
+		return this.title;
+	}
+
+	public String getDescription() {
+		return this.description;
+	}
+
+	public String getImage() {
+		return this.image;
+	}
+
+	public LocalDate getComposedAt() {
+		return this.composedAt;
+	}
 }
