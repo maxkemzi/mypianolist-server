3aa6a1c89f7a7f3a1ebd55736609eedfbaa08885
Max Kyrychenko <iam.maxkyrychenko@gmail.com>
2025-02-07 11:05:46 +0200

    add GET method for /pieces/{id} endpoint

--- src/main/java/com/maxkemzi/mypianolist/piece/controller/PieceController.java
+++ src/main/java/com/maxkemzi/mypianolist/piece/controller/PieceController.java
@@ -1,6 +1,7 @@
 package com.maxkemzi.mypianolist.piece.controller;
 
 import java.util.Optional;
+import java.util.UUID;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -9,6 +10,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -78,5 +80,17 @@ public class PieceController {
 		PieceResponseDTO resDTO = new PieceResponseDTO(savedPiece);
 
 		return ResponseEntity.status(HttpStatus.CREATED).body(resDTO);
+	}
+
+	@GetMapping("/{id}")
+	public ResponseEntity<PieceResponseDTO> findById(@PathVariable("id") UUID id) {
+		Optional<Piece> piece = repository.findById(id);
+		if (piece.isEmpty()) {
+			throw new PieceDoesntExistException();
+		}
+
+		PieceResponseDTO resDTO = new PieceResponseDTO(piece.get());
+
+		return ResponseEntity.ok(resDTO);
 	}
 }
