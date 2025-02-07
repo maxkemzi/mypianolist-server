9e8a8c11b2c9cc4ca0584db02857d4dff82e82c8
Max Kyrychenko <iam.maxkyrychenko@gmail.com>
2025-02-07 09:58:51 +0200

    add more request body validations for piece and composer

--- src/main/java/com/maxkemzi/mypianolist/composer/controller/ComposerDTO.java
+++ src/main/java/com/maxkemzi/mypianolist/composer/controller/ComposerDTO.java
@@ -2,23 +2,33 @@ package com.maxkemzi.mypianolist.composer.controller;
 
 import java.time.LocalDate;
 
+import org.hibernate.validator.constraints.URL;
+
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Size;
 
 public class ComposerDTO {
 	@NotBlank(message = "First name is required.")
+	@Size(min = 2, max = 50, message = "First name must be between 2 and 50 characters.")
 	private String firstName;
 
 	@NotBlank(message = "Last name is required.")
+	@Size(min = 2, max = 50, message = "Last name must be between 2 and 50 characters.")
 	private String lastName;
 
+	@Size(max = 50, message = "Nickname must be at most 50 characters.")
 	private String nickname;
 
 	@NotBlank(message = "Biography is required.")
+	@Size(min = 10, max = 2000, message = "Biography must be between 10 and 2000 characters.")
 	private String biography;
 
+	@URL(message = "Photo must be a valid URL.")
 	private String photo;
 
+	@NotNull(message = "Born date is required.")
 	@Past(message = "Born date must be in the past.")
 	private LocalDate bornAt;
 
--- src/main/java/com/maxkemzi/mypianolist/piece/controller/PieceDTO.java
+++ src/main/java/com/maxkemzi/mypianolist/piece/controller/PieceDTO.java
@@ -3,19 +3,26 @@ package com.maxkemzi.mypianolist.piece.controller;
 import java.time.LocalDate;
 import java.util.UUID;
 
+import org.hibernate.validator.constraints.URL;
+
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Size;
 
 public class PieceDTO {
 	@NotBlank(message = "Title is required.")
+	@Size(min = 2, max = 100, message = "Title must be between 2 and 100 characters.")
 	private String title;
 
 	@NotBlank(message = "Description is required.")
+	@Size(min = 10, max = 1000, message = "Description must be between 10 and 1000 characters.")
 	private String description;
 
+	@URL(message = "Image must be a valid URL.")
 	private String image;
 
+	@NotNull(message = "Composed date is required.")
 	@Past(message = "Composed date must be in the past.")
 	private LocalDate composedAt;
 
