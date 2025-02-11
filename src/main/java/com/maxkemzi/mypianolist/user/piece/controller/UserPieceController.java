4b941065e0f92d7e7ba7b48f7189c4ef4bb5110f
Max Kyrychenko <iam.maxkyrychenko@gmail.com>
2025-02-11 09:51:31 +0200

    add DELETE method for /users/{username}/pieces/{id} endpoint

--- src/main/java/com/maxkemzi/mypianolist/user/piece/controller/UserPieceController.java
+++ src/main/java/com/maxkemzi/mypianolist/user/piece/controller/UserPieceController.java
@@ -1,6 +1,7 @@
 package com.maxkemzi.mypianolist.user.piece.controller;
 
 import java.util.Optional;
+import java.util.UUID;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -8,6 +9,7 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -73,5 +75,12 @@ public class UserPieceController {
 		Page<UserPieceResponseDTO> resPage = page.map(UserPieceResponseDTO::new);
 
 		return new PageResponseDTO<>(resPage);
+	}
+
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> deleteById(@PathVariable("id") UUID id) {
+		repository.deleteById(id);
+
+		return ResponseEntity.noContent().build();
 	}
 }
