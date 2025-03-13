560074b19072379eb412226788986de84d117987
Max Kyrychenko <iam.maxkyrychenko@gmail.com>
2025-03-13 18:54:17 +0200

    complete testing for JwtService

--- src/test/java/com/maxkemzi/mypianolist/auth/service/jwt/JwtServiceTests.java
+++ src/test/java/com/maxkemzi/mypianolist/auth/service/jwt/JwtServiceTests.java
@@ -1,18 +1,14 @@
 package com.maxkemzi.mypianolist.auth.service.jwt;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
-@TestPropertySource(properties = {
-		"jwt.access-token.key=lujtTkJ8U2cHyVFunP1Mj2+EmkQG7IveF1/HpKOW2OQ=",
-		"jwt.refresh-token.key=Ax46SgFxobarXiFmiauIBZbf24ABZcsfkW9pNPWoCkA="
-})
 public class JwtServiceTests {
 	private final JwtService service;
 
@@ -26,12 +22,39 @@ public class JwtServiceTests {
 		JwtUser payload = new JwtUser("username", "avatar");
 		JwtTokens tokens = service.generateAccessAndRefreshTokens(payload);
 
+		String accessToken = tokens.getAccess();
+		String refreshToken = tokens.getRefresh();
+
 		assertNotNull(tokens, "Tokens should not be null.");
 
-		assertNotNull(tokens.getAccess(), "Access token should not be null.");
-		assertFalse(tokens.getAccess().isEmpty(), "Access token should not be empty.");
+		assertNotNull(accessToken, "Access token should not be null.");
+		assertFalse(accessToken.isEmpty(), "Access token should not be empty.");
 
-		assertNotNull(tokens.getRefresh(), "Refresh token should not be null.");
-		assertFalse(tokens.getRefresh().isEmpty(), "Refresh token should not be empty.");
+		assertNotNull(refreshToken, "Refresh token should not be null.");
+		assertFalse(refreshToken.isEmpty(), "Refresh token should not be empty.");
+	}
+
+	@Test
+	public void testVerifyAccessToken() {
+		JwtUser expected = new JwtUser("username", "avatar");
+		JwtTokens tokens = service.generateAccessAndRefreshTokens(expected);
+
+		JwtUser actual = service.verifyAccessToken(tokens.getAccess());
+
+		assertNotNull(actual, "Payload should not be null.");
+		assertEquals(expected.getUsername(), actual.getUsername(), "Usernames should match.");
+		assertEquals(expected.getAvatar(), actual.getAvatar(), "Avatars should match.");
+	}
+
+	@Test
+	public void testVerifyRefreshToken() {
+		JwtUser expected = new JwtUser("username", "avatar");
+		JwtTokens tokens = service.generateAccessAndRefreshTokens(expected);
+
+		JwtUser actual = service.verifyRefreshToken(tokens.getRefresh());
+
+		assertNotNull(actual, "Payload should not be null.");
+		assertEquals(expected.getUsername(), actual.getUsername(), "Usernames should match.");
+		assertEquals(expected.getAvatar(), actual.getAvatar(), "Avatars should match.");
 	}
 }
